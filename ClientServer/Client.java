d9a474bf00cae81bfaa457ca671577dcf6dca628
asibulhasanshanto <asibulhasan.rucse.18@gmail.com>
2021-12-20 11:03:25 +0600

    client server messae passing done

--- ClientServer/Client.java
+++ ClientServer/Client.java
@@ -6,13 +6,26 @@ public class Client{
             //connect to the socket
             Socket socket = new Socket("localhost",9000);
 
-            //write to server
-            DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
-            dout.writeUTF("Hello server");
-            dout.flush();
-            dout.close();
+            //create data input,output stream and a buffer
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            //create strings to store and send messages
+            String messageFromServer="",messageToServer="";
+
+            while(!messageFromServer.equals("END")){
+                System.out.print("From Client: ");
+                messageFromServer = bufferedReader.readLine();
+                dataOutputStream.writeUTF(messageFromServer);
+
+                messageToServer = dataInputStream.readUTF();
+                System.out.println("From Server: "+ messageToServer);
+            }
 
             //close the socket
+            dataOutputStream.flush();
+            dataOutputStream.close();
             socket.close();
         }catch(Exception e){
             System.out.println(e);
